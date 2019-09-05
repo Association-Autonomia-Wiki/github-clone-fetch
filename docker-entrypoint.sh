c488402504c3c404850cc61dc7087cb756293b9c
autonomia-wiki <54903378+autonomia-wiki@users.noreply.github.com>
2019-09-05 13:03:51 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-while true; do git clone https://github.com/wikimedia/mediawiki-extensions-CheckUser /github; sleep 600 ; done
+while true; do git clone https://github.com/wikimedia/mediawiki-extensions-CheckUser /github; sleep $SLEEP_TIME ; done
 
