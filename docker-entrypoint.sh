f8c8ca033a87f4609fd03539618c607b32def2e4
autonomia-wiki <54903378+autonomia-wiki@users.noreply.github.com>
2019-09-05 16:25:49 +0200

    Update docker-entrypoint.sh
    fix loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,14 +7,17 @@ do
   n=1
   name="GITHUB_NAME_${n}"
   eval name=\$$name
-  echo $name
   url="GITHUB_URL_${n}"
   eval url=\$$url
-  echo $url
   while [ ! -z ${name} ]
   do
+    echo $name - $url
     git -C $name pull || git clone $url $name
     ((n++))
+    name="GITHUB_NAME_${n}"
+    eval name=\$$name
+    url="GITHUB_URL_${n}"
+    eval url=\$$url
   done
   sleep $SLEEP_TIME
 done
