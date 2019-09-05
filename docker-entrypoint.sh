afcc20839de210087608a8c91e1f121b95d38357
autonomia-wiki <54903378+autonomia-wiki@users.noreply.github.com>
2019-09-05 15:59:34 +0200

    Update docker-entrypoint.sh
    fix variables use

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,12 @@ while true
 do
   n=1
   name="GITHUB_NAME_${n}"
+  eval name=\$$name
   echo $name
   url="GITHUB_URL_${n}"
+  eval url=\$$url
   echo $url
-  while [! -z "$name" ]
+  while [ -z ${name} ]
   do
     git -C $name pull || git clone $url $name
     ((n++))
