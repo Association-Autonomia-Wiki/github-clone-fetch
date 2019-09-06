23971f91d5658bb8d83573cb85ccb03bb1447ff0
autonomia-wiki <54903378+autonomia-wiki@users.noreply.github.com>
2019-09-06 10:57:01 +0200

    Update github-loop.sh
    add branch management

--- github-loop.sh
+++ github-loop.sh
@@ -11,15 +11,24 @@ do
   eval name=\$$name
   url="GITHUB_URL_${n}"
   eval url=\$$url
+  branch="GITHUB_BRANCH_${n}"
+  eval branch=\$$branch
   while [ ! -z ${name} ]
   do
     echo $name - $url
-    git -C $name pull || git clone $url $name
+    if [ -z ${branch} ]
+    then
+      git -v -C $name pull || git clone -v $url $name
+    else
+      git -v -C $name pull || git clone -v -b $branch $url $name
+    fi
     n=$((n+1))
     name="GITHUB_NAME_${n}"
     eval name=\$$name
     url="GITHUB_URL_${n}"
     eval url=\$$url
+    branch="GITHUB_BRANCH_${n}"
+    eval branch=\$$branch
   done
   sleep $SLEEP_TIME
 done
