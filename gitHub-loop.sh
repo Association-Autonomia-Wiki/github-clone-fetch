83d8011e71c639bb9cb6b62aa4eb6b499abe49cc
autonomia-wiki <54903378+autonomia-wiki@users.noreply.github.com>
2019-09-06 09:49:12 +0200

    Create gitHub-loop.sh

---
+++ gitHub-loop.sh
@@ -0,0 +1,23 @@
+
+
+echo user $user changed
+
+while true
+do
+  n=1
+  name="GITHUB_NAME_${n}"
+  eval name=\$$name
+  url="GITHUB_URL_${n}"
+  eval url=\$$url
+  while [ ! -z ${name} ]
+  do
+    echo $name - $url
+    git -C $name pull || git clone $url $name
+    n=$((n+1))
+    name="GITHUB_NAME_${n}"
+    eval name=\$$name
+    url="GITHUB_URL_${n}"
+    eval url=\$$url
+  done
+  sleep $SLEEP_TIME
+done
